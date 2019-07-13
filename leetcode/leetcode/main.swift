495a9b90d9cb1f5928aa8ffbe314617568f7f7c1
孟冰川 <mengbingchuan@36kr.com>
2019-07-13 14:57:27 +0800

    两数之和

---
+++ leetcode/leetcode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  leetcode
+//
+//  Created by 孟冰川 on 2019/7/13.
+//  Copyright © 2019 孟冰川. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ leetcode/leetcode/subject/TwoSum.swift
@@ -0,0 +1,25 @@
+//
+//  TwoSum.swift
+//  leetcode
+//
+//  Created by 孟冰川 on 2019/7/13.
+//  Copyright © 2019 孟冰川. All rights reserved.
+//
+
+import Foundation
+
+class Solution {
+    
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var map = [Int:Int]()
+        for i in 0...(nums.count - 1) {
+            let value = nums[i]
+            if let item = map.first(where: {$0.value == value}) {
+                return [item.key,i]
+            }else {
+                map[i] = target - value
+            }
+        }
+        return []
+    }
+}
